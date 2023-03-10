95751ff3e25dfc84cebef2cfdee83d2dc314e691
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-03-10 19:24:22 +0900

    Add files via upload

--- Section.04/src/components/NewExpense/NewExpense.js
+++ Section.04/src/components/NewExpense/NewExpense.js
@@ -1,3 +1,14 @@
-const NewExpense = () => {};
+import React from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import './NewExpense.css';
+
+const NewExpense = () => {
+    return (
+    <div className="new-expense">
+        <ExpenseForm />
+    </div>
+    );
+};
 
 export default NewExpense
