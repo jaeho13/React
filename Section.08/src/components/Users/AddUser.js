c6622ba400b47859d3a1aec6d12b4576448adb81
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-04-13 13:48:25 +0900

    Add files via upload

--- Section.08/src/components/Users/AddUser.js
+++ Section.08/src/components/Users/AddUser.js
@@ -5,23 +5,31 @@ import classes from "./AddUser.module.css";
 import Button from "../Ui/Button";
 import ErrorModal from "../Ui/ErrorModal";
 
-const AddUser = props => {
+const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name and age (non-empty values).'
+            });
             return;
         }
         if (+enteredAge < 1) {
+            setError({
+                title: 'Invalid age',
+                message: 'Please enter a valid age (>0 ).'
+            });
             return;
         }
-        props.onAdduser(enteredUsername, enteredAge);
+        props.onAddUser(enteredUsername, enteredAge);
         setEnteredUsername('');
         setEnteredAge('');
     };
-
 
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
@@ -31,9 +39,20 @@ const AddUser = props => {
         setEnteredAge(event.target.value);
     };
 
+    const erorrHandler = () => {
+        setError(null);
+    };
+
+
     return (
         <div>
-        <ErrorModal  title="An error occured!" message="Somgthing went wrong!" />
+        {error && (
+        <ErrorModal
+            title={error.title}
+            message={error.message}
+            onConfirm={erorrHandler}
+        />
+        )}
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
