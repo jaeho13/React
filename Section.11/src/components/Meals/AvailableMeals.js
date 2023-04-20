876b391ea733e066dc57efe4ac870dbfed383e2e
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-04-20 23:28:41 +0900

    Add files via upload

--- Section.11/src/components/Meals/AvailableMeals.js
+++ Section.11/src/components/Meals/AvailableMeals.js
@@ -1,4 +1,9 @@
+import Card from "../Ui/Card";
+import MealItem from "./MealItem/MealItem";
+
+
 import classes from "./AvailableMeals.module.css";
+
 
 const DUMMY_MEALS = [
     {
@@ -30,13 +35,22 @@ const DUMMY_MEALS = [
 
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map((meal) => (
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    ));
 
     return (
         <section className={classes.meals}>
+          <Card>
             <ul>
                 {mealsList}
             </ul>
+          </Card>
         </section>
     );
 };
