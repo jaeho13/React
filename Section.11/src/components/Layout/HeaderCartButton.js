afc2a74e7afbfb33c7ddcda8f08f5afceff51258
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-04-25 16:05:07 +0900

    Add files via upload

--- Section.11/src/components/Layout/HeaderCartButton.js
+++ Section.11/src/components/Layout/HeaderCartButton.js
@@ -3,15 +3,15 @@ import classes from "./HeaderCart.module.css"
 
 
 const HeaderCartButton = props => {
-    return <button className={classes.button}>
+    return (
+        <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>
-            3
-        </span>
+        <span className={classes.badge}>3</span>
     </button>
+    );
 };
 
 export default HeaderCartButton;
