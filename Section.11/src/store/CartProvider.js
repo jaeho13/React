cfa906a71cc973a8d7970854657012479b83ecca
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-05-02 23:53:23 +0900

    Add files via upload

--- Section.11/src/store/CartProvider.js
+++ Section.11/src/store/CartProvider.js
@@ -1,6 +1,8 @@
 import CartContext from "./cart-context";
 
 const CartProvider = props => {
+    const CartContext ={};
+
     return <CartContext.Provider>
         {props.children}
     </CartContext.Provider>
