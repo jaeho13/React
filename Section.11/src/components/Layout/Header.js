a0af687a537f4d2079abae762ab38135685d09c8
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-05-28 23:07:14 +0900

    Update Header.js

--- Section.11/src/components/Layout/Header.js
+++ Section.11/src/components/Layout/Header.js
@@ -5,7 +5,7 @@ import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
     return (
-    <Fragment>
+    <>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
             <HeaderCartButton onClick={props.onShowCart} />
@@ -13,7 +13,7 @@ const Header = (props) => {
         <div className={classes['main-image']} >
             <img src={mealsImage} alt='A table full of delicious food!' />
         </div>
-    </Fragment>
+    </>
     );
 };
 
