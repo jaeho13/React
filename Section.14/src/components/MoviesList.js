252b75f97856b4db89b5fb891857eb3b2c836e30
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-05-29 23:55:32 +0900

    Add files via upload

--- Section.14/src/components/MoviesList.js
+++ Section.14/src/components/MoviesList.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Movie from './Movie';
 import classes from './MoviesList.module.css';
 
-const MovieList = (props) => {
+const MovieList = () => {
   return (
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
