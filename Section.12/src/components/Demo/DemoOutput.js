9e2e2c3f2ce33180a2db61cc116e61f17599fd66
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-05-30 23:50:20 +0900

    Add files via upload

--- Section.12/src/components/Demo/DemoOutput.js
+++ Section.12/src/components/Demo/DemoOutput.js
@@ -1,7 +1,7 @@
 import React from "react"
 import MyParagraph  from "./MyParagraph";
 
-const DemoOutput = (props) => {
+const DemoOutput = () => {
     console.log('DemoOutput RUNNING')
     return <MyParagraph>{props.show ? 'This is new!' : ''} </MyParagraph>;
 };
